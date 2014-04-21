9f0461db56e7e0d74f6a37b416c118883d5d8f81
Himal Jayasuriya <himalj@reevestreet.com>
2014-04-22 00:47:45 +0530

    change of code

--- app.js
+++ app.js
@@ -4,28 +4,31 @@ var app = express();
 // Set up a URL route
 app.get("/", function(req, res) {
 	var options = {
-	  host: 'www.makeyourwidget.com',
+	  host: 'www.bbc.com',
 	  port: 80,
 	  path: '/',
 	  method: 'GET'
 	};
 	var statCode = false;
 	var http = require('http');
-	var req = http.request(options, function(res) {
-		if(res.statusCode == "200"){	
+	var extReq = http.request(options, function(res) {
+		if(extReq.statusCode == "200"){	
 			statCode = true;
+			console.log(statCode);
 		}
 		
 	});
-
+	extReq.on('error', function(e) {
+	  console.log('problem with request: ' + e.message);
+	});
+	extReq.end();
+	
 	if(statCode){
 		res.send("status up");
 	}
-	req.on('error', function(e) {
-	  console.log('problem with request: ' + e.message);
-	});
+	
 
-	req.end();
+	
 });
 
 // bind the app to listen for connections on a specified port
