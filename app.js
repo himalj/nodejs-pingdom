8167edd796ac6dc9c6895784a2c85abb73de1aae
Himal Jayasuriya <himalj@reevestreet.com>
2014-04-22 00:29:48 +0530

    addition of pingdom code

--- app.js
+++ app.js
@@ -3,7 +3,29 @@ var app = express();
 
 // Set up a URL route
 app.get("/", function(req, res) {
- res.send("Heroku Demo!");
+	var options = {
+	  host: 'www.makeyourwidget.com',
+	  port: 80,
+	  path: '/',
+	  method: 'GET'
+	};
+	var statCode = false;
+	var http = require('http');
+	var req = http.request(options, function(res) {
+		if(res.statusCode == "200"){	
+			statCode = true;
+		}
+		
+	});
+
+	if(statCode){
+		res.send("status up");
+	}
+	req.on('error', function(e) {
+	  console.log('problem with request: ' + e.message);
+	});
+
+	req.end();
 });
 
 // bind the app to listen for connections on a specified port
